393744467a608e36f4b2aeec0b2ba751821bd173
DMirkarimi <plppl.gaming@gmail.com>
2021-05-12 14:34:36 +0200

    done

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
+import re
 from flask import Flask, render_template, request
 from Bio.Seq import Seq
+from Bio.Blast import NCBIWWW, NCBIXML
 
 app = Flask(__name__)
+
+
+def seq_info(sequence: Seq) -> str:
+    if sequence.seq_type == 'DNA':
+        rna = sequence.transcribe()
+        protein = sequence.translate()
+
+        text = f'Sequence has been classified as DNA\n' \
+               f'Transribed: {rna}\n' \
+               f'Translated: {protein}'
+        return text
+    elif sequence.seq_type == 'RNA':
+        return 'Sequence contains RNA'
+    else:
+        result = NCBIWWW.qblast(program="blastp", database="nr",
+                                sequence=str(sequence), ncbi_gi=True,
+                                hitlist_size=1, format_type='XML',
+                                matrix_name='BLOSUM62', gapcosts='11 1',
+                                word_size=6)
+        try:
+            alignment = NCBIXML.read(result).alignments[0]
+            accession_code = alignment.accession
+        except (IndexError, AttributeError):
+            accession_code = 'Not found'
+
+        text = f'Protein detected\n' \
+               f'Most likely parent gene: {accession_code}'
+        return text
 
 
 @app.route('/')
@@ -16,16 +46,27 @@ def index() -> str:
 
 @app.route("/convert", methods=['POST', 'GET'])
 def convert() -> str:
-    dna = request.form.get('seq')
+    seq = request.form.get('seq')
 
-    if dna:
+    if seq:
         try:
-            sequence = Seq(str(dna))
+            seq_type = None
+            sequence = str(seq)
+            if not re.search(r'[^ATGC]', sequence):
+                seq_type = 'DNA'
+            elif not re.search(r'[^AUGC]', sequence):
+                seq_type = 'RNA'
+            elif not re.search(r'[^AC-IK-NP-Y]', seq):
+                seq_type = 'protein'
+
+
+            seq_obj = Seq(sequence)
+            seq_obj.seq_type = seq_type
             return render_template('convert.html',
-                                   protein=sequence.translate())
+                                   output=seq_info(seq_obj))
         except:
             return render_template('convert.html',
-                                   protein='Error')
+                                   output='Error')
     else:
         return render_template('convert.html')
 
